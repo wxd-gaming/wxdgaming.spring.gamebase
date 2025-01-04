5292020f3b235d02a16187c92cdd9c962fe2ba3a
wxdgaming <492794628@qq.com>
2025-01-04 20:43:20 +0800

    【重构】优化

--- background/src/test/java/push/TestBase.java
+++ background/src/test/java/push/TestBase.java
@@ -1,21 +1,33 @@
 package push;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import wxdgaming.spring.boot.core.CoreScan;
 import wxdgaming.spring.boot.core.lang.RunResult;
-import wxdgaming.spring.boot.core.threading.ExecutorBuilder;
+import wxdgaming.spring.boot.core.threading.VirtualExecutor;
 import wxdgaming.spring.boot.webclient.HttpClientBuild;
 import wxdgaming.spring.boot.webclient.HttpClientService;
 import wxdgaming.spring.boot.webclient.HttpPostTextWork;
+import wxdgaming.spring.boot.webclient.WebClientScan;
 
+@RunWith(SpringRunner.class)
+@SpringBootApplication
+@SpringBootTest(classes = {CoreScan.class, WebClientScan.class})
 public class TestBase {
 
+    @Autowired
     protected HttpClientService httpClientService;
 
     @Before
     public void before() {
-        ExecutorBuilder executorBuilder = new ExecutorBuilder();
-        HttpClientBuild httpClientBuild = new HttpClientBuild();
-        httpClientService = new HttpClientService(executorBuilder.virtualExecutor(), httpClientBuild.httpClient());
+        // VirtualExecutor virtualExecutor = new VirtualExecutor(200);
+        // HttpClientBuild httpClientBuild = new HttpClientBuild();
+        // httpClientService = new HttpClientService(virtualExecutor, httpClientBuild.httpClient());
     }
 
     public String getToken() {
